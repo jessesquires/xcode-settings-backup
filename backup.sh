9e7b8e6d363d22e5bbb63824eb4b3eeb48ab35ad
Jesse Squires <jesse@jessesquires.com>
2017-07-30 17:57:14 -0700

    update. add template macro (xcode9)

--- backup.sh
+++ backup.sh
@@ -2,3 +2,4 @@
 
 cp ~/Library/Developer/Xcode/UserData/FontAndColorThemes/*.dvtcolortheme .
 cp ~/Library/Developer/Xcode/UserData/FontAndColorThemes/*.xccolortheme .
+cp ~/Library/Developer/Xcode/UserData/IDETemplateMacros.plist .
--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ DIR=~/Library/Developer/Xcode/UserData/FontAndColorThemes/
 mkdir -p $DIR
 cp ./*.dvtcolortheme $DIR
 cp ./*.xccolortheme $DIR
+
+cp ./IDETemplateMacros.plist ~/Library/Developer/Xcode/UserData/IDETemplateMacros.plist
