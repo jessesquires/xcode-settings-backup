e9a5b19edb4bb1d2e854ae5e122d4ee6265ecd9f
Jesse Squires <jesse@jessesquires.com>
2020-06-30 00:18:49 -0700

    update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,14 +4,19 @@ USER="$(whoami)"
 XC_USER_DATA="/Users/$USER/Library/Developer/Xcode/UserData"
 
 # color theme
+echo 'Copying color themes...'
 THEME_DIR="$XC_USER_DATA/FontAndColorThemes/"
 mkdir -p $THEME_DIR
 cp ./*.xccolortheme $THEME_DIR
 
 # template
+echo 'Copying header templates...'
 cp ./IDETemplateMacros.plist "$XC_USER_DATA"
 
 # breakpoints
+echo 'Copying breakpoints...'
 BP_DIR="$XC_USER_DATA/xcdebugger/"
 mkdir -p $BP_DIR
 cp ./Breakpoints_v2.xcbkptlist $BP_DIR
+
+echo 'Done!'
